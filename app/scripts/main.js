507fc46b9a8644183c67a8b63ff76a0f54209108
Kevin Bost <kevbost@gmail.com>
2016-01-03 10:52:22 -0500

    pure insanity, applied snabbt animation json cut up with blast-text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
 console.log('\'Allo \'Allo!');
+
+$(function(){
+    $("div").blast({
+        delimiter: "letter",
+        tag: "span"
+    });
+
+    snabbt(document.querySelectorAll('.blast'), {
+        fromRotation: [0, 0, 0],
+        rotation: function(i, total) {
+            return [0, 0, (i / (total - 1)) * (Math.PI / 2)];
+        },
+        delay: function(i) {
+            return i * 20;
+        },
+        easing: 'spring',
+    }).snabbt({
+        rotation: [0, 0, 0],
+        delay: function(i, total) {
+            return (total - i - 1) * 10;
+        },
+        easing: 'ease',
+    });
+})
